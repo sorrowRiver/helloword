655513c3beaf71d0088df8b077f8ecf3ed1f7267
sorrowRiver <liyongyansi1322@126.com>
2017-07-06 15:05:18 +0800

    add poj

---
+++ hello/src/com/gg/hub/TestHello.java
@@ -0,0 +1,16 @@
+package com.gg.hub;
+/**
+ * @author ly
+ * @date 2017年7月6日 
+ * 
+ */
+
+public class TestHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("11");
+	}
+
+}
